06100175d7b1413ae114b7326f7273d1eb3b3293
Davis, Tony <tony.davis@jhuapl.edu>
2024-02-08 02:28:41 +0000

    upload all docs, emoji response???

--- app2.py
+++ app2.py
@@ -5,24 +5,26 @@ import streamlit as st
 
 from bravobot import ChatBot, DocumentLoader, VectorStoreManager
 
+model_path = '/home/pvcdata/bravo11bot/mistral/llama2_13b_chat.gguf'
+
 # Initialize Streamlit app
 st.title("Document-based Q&A System")
 
 try:
-    vector_store = VectorStoreManager(faiss_index='/home/pvcdata/bravo11bot/faiss_index')
+    vector_store = VectorStoreManager(faiss_index_path='/home/pvcdata/bravo11bot/faiss_index').create_vector_store()
+    st.success("Vector store found and loaded from disk (faiss_index)")
 except FileNotFoundError:
     # Streamlit interface to allow users to upload PDF files
     uploaded_files = st.file_uploader("Upload PDF, Excel, or JSON documents", accept_multiple_files=True)
 
-# Temporary directory for uploaded files
-temp_dir = tempfile.mkdtemp()
+    # Temporary directory for uploaded files
+    temp_dir = tempfile.mkdtemp()
 
-# Placeholder for documents and vector store
-documents = []
-vector_store = None
+    # Placeholder for documents and vector store
+    documents = []
+    vector_store = None
 
 # Process uploaded files and load/create vector store
-if uploaded_files:
     for uploaded_file in uploaded_files:
         with open(os.path.join(temp_dir, uploaded_file.name), "wb") as f:
             f.write(uploaded_file.getbuffer())
@@ -30,7 +32,6 @@ if uploaded_files:
         document_loader = DocumentLoader(pdf_path)
         documents.extend(document_loader.load_documents())
     if documents:
-        model_path = '/home/pvcdata/bravo11bot/mistral/llama2_13b_chat.gguf'  # Update this path
         vector_store_manager = VectorStoreManager(documents, model_path)
         vector_store = vector_store_manager.create_vector_store()
         st.success("Documents processed and vector store created/loaded.")
@@ -39,9 +40,11 @@ if uploaded_files:
 chat_bot = ChatBot(vector_store, model_path)
 
 # Streamlit interface for user to input a question
+count = 0
 while True:
-    question = st.text_input("Type your question here:")
-    if question:
+    question = st.text_input("Type your question here:", key=count)
+    if question != '':
+        count += 1
         # Perform search and generate answer
         response, context = chat_bot.chat(question)
         st.markdown("### Answer")
